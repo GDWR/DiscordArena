ba06bdac72c7a7559a52f3b3854f2c465960fca0
jakea <jjakeamonck@gmail.com>
2021-04-16 14:32:10 -0400

    Added sync to commands

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -9,7 +9,7 @@ from discord_slash import SlashCommand
 from arena_bot import ArenaBot
 
 bot = commands.Bot(command_prefix="!")
-slash = SlashCommand(bot, override_type=True)
+slash = SlashCommand(bot, sync_commands=True, override_type=True)
 
 for file in map(Path, glob("bot/cogs/*.py")):
     bot.load_extension(f"cogs.{file.stem}")

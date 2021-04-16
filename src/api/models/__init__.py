884e96bc38c22085a0007464436b6fd2591568ea
ForbiddenKJ <jamesbutcher167@gmail.com>
2021-04-16 22:43:24 +0100

    Changed RNG class to use enums and simplify rolling

--- src/api/models/__init__.py
+++ src/api/models/__init__.py
@@ -1,1 +1,2 @@
 from .player import PlayerDB, PlayerDTO
+from .rarity import Rarity
--- src/api/services/rng.py
+++ src/api/services/rng.py
@@ -1,53 +1,29 @@
 from random import choices
-
+from models import Rarity
+from typing import Union
 
 class RNG:
-    rarities: list[str] = [
-        "Poor",
-        "Common",
-        "Uncommon",
-        "Rare",
-        "Epic",
-        "Legendary",
-        "Mythical",
-    ]
+    def __init__(self, weights: Union[Rarity, list[int]] = None):
+        if weights is None:
+            self.weights = Rarity.Poor.value
 
-    def poor(
-        weights: list[int] = [50.077, 27.337, 16.916, 4.787, 0.815, 0.066, 0.002],
-        rolls: int = 1,
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
+        else:
+            if isinstance(weight, Rarity):
+                self.weights = weights.value
 
-    def common(
-        weights: list[int] = [27.738, 49.889, 16.781, 4.732, 0.777, 0.078, 0.005],
-        rolls: int = 1,
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
+            elif isinstance(weights, list):
+                self.weights = weights
 
-    def uncommon(
-        weights: list[int] = [10.5, 23.031, 55.163, 9.665, 1.455, 0.18, 0.006],
-        rolls: int = 1,
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
+        self.rewards: list[Rarity] = [
+            Rarity.Poor,
+            Rarity.Common,
+            Rarity.Uncommon,
+            Rarity.Rare,
+            Rarity.Epic,
+            Rarity.Legendary,
+            Rarity.Mythical,
+        ]
 
-    def rare(
-        weights: list[int] = [0.0, 0.0, 11.403, 74.866, 13.362, 0.351, 0.018],
-        rolls: int = 1,
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
 
-    def epic(
-        weights: list[int] = [0.0, 0.0, 0.0, 85.014, 13.451, 1.465, 0.07],
-        rolls: int = 1,
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
-
-    def legendary(
-        weights: list[int] = [0.0, 0.0, 0.0, 10.0, 79.979, 9.509, 0.512], rolls: int = 1
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
-
-    def mythical(
-        weights: list[int] = [0.0, 0.0, 0.0, 0.0, 10.0, 84.932, 5.068], rolls: int = 1
-    ) -> list[str]:
-        return choices(RNG.rarities, weights, k=rolls)
+    def roll(self, k: int = 1) -> Rarity:
+        return choices(self.rewards, self.weights, k=k)

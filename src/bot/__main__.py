b6f8618dadeb4bf7b9ba743aea417c35d64b592e
DudeBro249 <appdevdeploy@gmail.com>
2021-04-17 16:08:28 +0530

    create classmethod for bot initialization

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -1,18 +1,14 @@
-import discord
-from discord.ext import commands
-
-from glob import glob
-from pathlib import Path
-from constants import environment
+from bot.constants import environment
 from discord_slash import SlashCommand
 
-from arena_bot import ArenaBot
+from bot.arena_bot import ArenaBot
 
-bot = commands.Bot(command_prefix="!")
-slash = SlashCommand(bot, sync_commands=True, override_type=True)
+def main() -> None:
+    bot = ArenaBot.create()
+    SlashCommand(bot, sync_commands=True, override_type=True)
 
-for file in map(Path, glob("bot/cogs/*.py")):
-    bot.load_extension(f"cogs.{file.stem}")
+    bot.load_extensions()
+    bot.run(environment.TOKEN)
 
-
-bot.run(environment.TOKEN)
+if __name__ == "__main__":
+    main()
--- src/bot/arena_bot.py
+++ src/bot/arena_bot.py
@@ -1,11 +1,16 @@
+from __future__ import annotations
+
+from glob import glob
+from pathlib import Path
+
 import aiohttp
+import discord
 from discord.ext.commands import Bot
 
 
 class ArenaBot(Bot):
     def __init__(self, command_prefix, *args, **kwargs):
         super().__init__(command_prefix, *args, **kwargs)
-
         self.session = aiohttp.ClientSession()
 
     async def on_ready(self):
@@ -13,3 +18,16 @@ class ArenaBot(Bot):
 
     async def on_disconnect(self):
         await self.session.close()
+
+    def load_extensions(self) -> None:
+        for file in map(Path, glob("bot/cogs/*.py")):
+            self.load_extension(f"bot.cogs.{file.stem}")
+
+    @classmethod
+    def create(cls) -> ArenaBot:
+        bot_intents = discord.Intents.default()
+        bot = cls(
+            command_prefix='!',
+            intents=bot_intents
+        )
+        return bot

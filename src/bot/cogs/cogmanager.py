8875b4cf2968b99420a7ee52f9cf49850bdc8677
Jake <77035482+Slushs@users.noreply.github.com>
2021-04-17 10:25:34 -0400

    Update cogmanager.py

--- src/bot/cogs/cogmanager.py
+++ src/bot/cogs/cogmanager.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Cog, group, Context,ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError, \
+from discord.ext.commands import Cog, group, Context, ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError, \
     ExtensionFailed, ExtensionNotLoaded
 
 

0262d37d1ba32f1dbf36044e8ba37f4611fd34f1
KJ002 <jamesbutcher167@gmail.com>
2021-06-13 20:43:37 +0100

    Add logging

--- bot/arena_bot.py
+++ bot/arena_bot.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 from glob import glob
 from pathlib import Path
+import logging
 
 from aiohttp import ClientSession
 from discord import Intents
@@ -31,22 +32,25 @@ class ArenaBot(Bot):
 
     def __init__(self, command_prefix: str, *args, **kwargs):
         super().__init__(command_prefix, *args, **kwargs)
+        self.logger = logging.getLogger("ArenaBot")
         self.session = ClientSession()
         self.database = Database()
         asyncio.ensure_future(self.database.connect())
 
     async def on_ready(self) -> None:
         """Task when the bot logs in and is connected to the gateway."""
-        print(f"Logged in as {self.user}")
+        self.logger.info(f"Bot Logged in as {self.user}")
 
     async def on_disconnect(self) -> None:
         """Task when the bot disconnects from the gateway."""
+        self.logger.info("Bot Disconnected")
         await self.session.close()
 
     def load_extensions(self) -> None:
         """Iterate through all the `.py` files found int `bot/cogs` and load them as cogs."""
         for file in map(Path, glob("bot/cogs/*.py")):
             self.load_extension(f"cogs.{file.stem}")
+            self.logger.info(f"Loaded cogs.{file.stem}")
 
     @classmethod
     def create(cls) -> ArenaBot:

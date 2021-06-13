a1cc57e1972c2ed423941f5204ab4618ecee2538
James <36408549+KJ002@users.noreply.github.com>
2021-06-14 00:30:36 +0100

    Add levelname to format

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,7 +3,7 @@ import logging
 from config import LOG_LEVEL
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s: %(message)s",
+    format="%(asctime)s - %(levelname) - %(name)s: %(message)s",
     datefmt="%d-%m-%Y %H:%M:%S",
     level=LOG_LEVEL,
 )

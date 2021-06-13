920081e271d98caa172fa7d16e542439f096cb20
KJ002 <jamesbutcher167@gmail.com>
2021-06-13 23:14:16 +0100

    Re-Formatting

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,4 +2,8 @@ import logging
 
 from config import LOG_LEVEL
 
-logging.basicConfig(format='%(asctime)s - %(name)s: %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=LOG_LEVE)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s: %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+    level=LOG_LEVEL,
+)
